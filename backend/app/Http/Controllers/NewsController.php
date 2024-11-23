b3fed0f5fa1f0e0664b5c5bd7543ac43c01b3e82
Maya Eid <mayabaseleid@gmail.com>
2024-11-23 08:57:58 +0200

    Create get news API

--- backend/app/Http/Controllers/NewsController.php
+++ backend/app/Http/Controllers/NewsController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\News;
 
 class NewsController extends Controller
 {
     function get_news(){
+        $news = News::all();
         return response()->json([
-            "message"=>"Hello World!"
+            "news"=>$news
         ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
 
-Route::get('/test', [NewsController::class, "test_api"]);
+Route::get('/news', [NewsController::class, "get_news"]);
